c3329bb2eda599ad83c021a4a78ec8f087fefa86
yonghwan <dyd7199@naver.com>
2024-05-16 20:26:40 +0900

    [실버] 큐 / 큐

---
+++ BaekJoon/Queue.java
@@ -0,0 +1,89 @@
+package baekjoon;
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static class Queue {
+
+
+        LinkedList<Integer> queue;
+
+        public Queue() {
+            queue = new LinkedList<>();
+        }
+
+        public int empty(){
+            if (queue.isEmpty()){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        public void push(int value){
+            queue.add(value);
+        }
+
+        public int pop(){
+
+            if (queue.isEmpty()){
+                return -1;
+            }
+            return queue.pollFirst();
+        }
+
+        public int size(){
+            return queue.size();
+        }
+        public int front(){
+            if (queue.isEmpty()){
+                return -1;
+            }
+
+            return queue.peekFirst();
+        }
+
+        public int back(){
+            if (queue.isEmpty()){
+                return -1;
+            }
+            return queue.peekLast();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        Queue queue = new Queue();
+
+        for (int i = 0; i < N; i ++){
+            String str = br.readLine();
+
+            String[] s = str.split(" ");
+
+            if ("push".equals(s[0])){
+                queue.push(Integer.parseInt(s[1]));
+            } else if ("pop".equals(s[0])) {
+                sb.append(queue.pop()).append("\n");
+            } else if ("size".equals(s[0])) {
+                sb.append(queue.size()).append("\n");
+            } else if ("empty".equals(s[0])) {
+                sb.append(queue.empty()).append("\n");
+            } else if ("front".equals(s[0])) {
+                sb.append(queue.front()).append("\n");
+            } else if ("back".equals(s[0])) {
+                sb.append(queue.back()).append("\n");
+            }
+        }
+        System.out.println(sb);
+
+
+    }
+}
+
