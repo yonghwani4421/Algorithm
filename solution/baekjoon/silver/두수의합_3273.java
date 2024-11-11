f0d6c64e751ff32111b158c67074d4606bc8a157
김용환 <ghks4421@gmail.com>
2024-11-11 16:22:52 +0900

    [백준] 두수의합

---
+++ solution/baekjoon/silver/두수의합_3273.java
@@ -0,0 +1,62 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 설명
+ *
+ * n개의 서로 다른 양의 정수 a1, a2, ..., an으로 이루어진 수열이 있다. ai의 값은 1보다 크거나 같고, 1000000보다 작거나 같은 자연수이다.
+ * 자연수 x가 주어졌을 때, ai + aj = x (1 ≤ i < j ≤ n)을 만족하는 (ai, aj)쌍의 수를 구하는 프로그램을 작성하시오.
+ *
+ *
+ */
+
+public class 두수의합_3273 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int x = Integer.parseInt(bf.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+
+        int rt = n-1, lt = 0;
+
+        while(rt > lt){
+
+            if (arr[lt] + arr[rt] == x){
+                answer++;
+            }
+            if (arr[lt] + arr[rt] <= x) {
+                lt++;
+            } else {
+                rt--;
+            }
+
+        }
+
+
+
+
+        System.out.println(answer);
+    }
+
+
+
+
+
+}
