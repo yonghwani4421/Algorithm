4dc1823ace2d73527814cb5afbba0db1f9a692c4
김용환 <ghks4421@gmail.com>
2024-11-25 18:27:12 +0900

    [인프런] 선택정렬 / 버블정렬

--- solution/inflearn/sort/버블정렬.java
+++ solution/inflearn/sort/버블정렬.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 선택정렬 {
+public class 버블정렬 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -22,26 +22,23 @@ public class 선택정렬 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 선택정렬
+        // 버블정렬
         int temp = 0;
-
-        for (int i = 0; i < N-1; i++) {
-            int idx = i;
+        for (int i = 0; i < N; i++) {
             for (int j = i+1; j < N; j++) {
-                if (arr[j] < arr[idx]){
-                    idx = j;
+                if (arr[i] > arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
-
-            temp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = temp;
 
         }
 
         for (int i : arr) {
             System.out.print(i + " ");
         }
+
 
     }
 }
